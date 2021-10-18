94d6c5c5611d74a6dc65e4157738204fc2a43330
tmedcount <83993395+tmedcount@users.noreply.github.com>
2021-10-18 19:52:28 +0900

    추상메소드

--- src/part3/ex5/abstract_/NewlecExam.java
+++ src/part3/ex5/abstract_/NewlecExam.java
@@ -2,4 +2,43 @@ package part3.ex5.abstract_;
 
 public class NewlecExam extends Exam {
 	
+	private int com;
+	
+	public NewlecExam() {
+		this(0,0,0,0);
+	}
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		super(kor, eng, math);
+		this.com = com;
+	}
+		
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+	
+	/*
+	@Override
+	public int total() {
+		int total = getKor() + getEng() + getMath() + getCom();
+		return total;
+	}
+	*/
+	
+	@Override
+	public int total() {
+		int total = onTotal() + getCom();
+		return total;
+	}
+	
+	@Override
+	public float avg() {
+		
+		return total()/4.0f;
+	}
+	
 }
