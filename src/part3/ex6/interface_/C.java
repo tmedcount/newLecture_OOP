b8e463ea751e929995e38dea69d8159876063a34
yamto <tmedcount@gmail.com>
2021-10-19 13:26:55 +0900

    인터페이스3(문자열 읽어서 객체로 만들기)

---
+++ src/part3/ex6/interface_/C.java
@@ -0,0 +1,11 @@
+package part3.ex6.interface_;
+
+public class C implements X {
+
+	@Override
+	public int total() {
+		
+		return 50;
+	}
+
+}
--- src/part3/ex6/interface_/Program.java
+++ src/part3/ex6/interface_/Program.java
@@ -1,14 +1,33 @@
 package part3.ex6.interface_;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		FileInputStream fis = new FileInputStream("src/part3/ex6/interface_/setting.txt");
+		Scanner scan = new Scanner(fis);
+		
+		String className = scan.nextLine();
+		
+		// System.out.println(className);
+		
+		scan.close();
+		fis.close();
+		
+		// A.class.newInstance(); = new A();
+ 		
+		Class clazz = Class.forName(className);
 		A a = new A();
-		B b = new B();
-		
-		a.setX(b);
-		
+		X x = (X) clazz.newInstance();
+		  
+		a.setX(x);
+		  
 		a.print();
+		 
 		
 	}
 
