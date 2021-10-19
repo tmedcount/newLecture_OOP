5fcb0d48530041a178f48430ecfd13e2f502a423
Bae Jin Ryeol <qowlsfuf11@naver.com>
2021-10-19 09:16:28 +0900

    이벤트메소드

--- src/part3/ex5/abstract_/NewlecExamConsole.java
+++ src/part3/ex5/abstract_/NewlecExamConsole.java
@@ -1,11 +1,40 @@
 package part3.ex5.abstract_;
 
-public class NewlecExamConsole extends ExamConsole {
+import java.util.Scanner;
 
+public class NewlecExamConsole extends ExamConsole {
+	
 	@Override
 	protected Exam makeExam() {
 		
 		return new NewlecExam();
+	}
+
+	@Override
+	protected void onPrint(Exam exam) {
+		NewlecExam newlecExam = (NewlecExam)exam;
+		int com = newlecExam.getCom();
+		System.out.printf("컴퓨터: %d\n", com);
+		
+	}
+
+	@Override
+	protected void onInput(Exam exam) {
+		NewlecExam newlecExam = (NewlecExam)exam;
+		
+		Scanner scan = new Scanner(System.in);
+		int com;
+		
+		do {
+	        System.out.printf("컴퓨터: ");
+	        com = scan.nextInt();
+        
+	        if(com < 0 || 100 < com)
+	        	System.out.println("컴퓨터성적은 0~100까지의 범위만 가능합니다.");
+	        
+        } while(com < 0 || 100 < com);
+		
+		newlecExam.setCom(com);
 	}
 
 }
